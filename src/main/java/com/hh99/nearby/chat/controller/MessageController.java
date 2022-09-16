74bcc9c060ecb462fedef9658106263cced54b77
Oh-Myeongjae <omjcws@gmail.com>
2022-09-16 23:48:14 +0900

    feat(실시간채팅): 실시간채팅입장시 기존인원 조회

--- src/main/java/com/hh99/nearby/chat/controller/MessageController.java
+++ src/main/java/com/hh99/nearby/chat/controller/MessageController.java
@@ -1,17 +1,26 @@
 package com.hh99.nearby.chat.controller;
 
 
-
 import com.hh99.nearby.chat.dto.ChatMessage;
+import com.hh99.nearby.chat.dto.SessionMemberDto;
 import com.hh99.nearby.chat.entity.Chat;
 import com.hh99.nearby.chat.repository.ChatRepository;
+import com.hh99.nearby.entity.Member;
+import com.hh99.nearby.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +29,8 @@ public class MessageController {
     private final SimpMessageSendingOperations sendingOperations; // @EnableWebSocketMessageBroker를 통해서 등록되는 Bean이다. Broker로 메시지를 전달한다.
 
     private final ChatRepository chatRepository;
+
+    private final MemberRepository memberRepository;
 
 
     @MessageMapping("/chat/message") //클라이언트가 Send 할수 있는 경로
@@ -42,5 +53,20 @@ public class MessageController {
             message.setMessage(message.getMessage()+message.getSendTime());
         }
         sendingOperations.convertAndSend("/sub/chat/room/"+message.getRoomId(),message);
+    }
+
+    @GetMapping("/api/chat/list/{challenge_id}") //클라이언트가 Send 할수 있는 경로
+    public ResponseEntity<?> PeopleList(@PathVariable Long challenge_id) {
+        List<Chat> chatList = chatRepository.findAllByChallengeId(challenge_id);//특정 챌린지에 참여하고 있는 채팅수
+        List<SessionMemberDto> list = new ArrayList<SessionMemberDto>();//지금까지 특정 챌린지참여 하고 있는 맴버 정보 만들기 위한 리스트 선언
+        for(Chat chat: chatList){
+            Optional<Member> member = memberRepository.findByNickname(chat.getSender());
+            SessionMemberDto sessionMemberDto = SessionMemberDto.builder()
+                    .level("0 LV")
+                    .nickname(member.get().getNickname())
+                    .build();
+            list.add(sessionMemberDto);
+        }
+        return ResponseEntity.ok().body(Map.of("msg", "조회완료", "data", list));
     }
 }
--- src/main/java/com/hh99/nearby/chat/repository/ChatRepository.java
+++ src/main/java/com/hh99/nearby/chat/repository/ChatRepository.java
@@ -3,6 +3,10 @@ package com.hh99.nearby.chat.repository;
 import com.hh99.nearby.chat.entity.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRepository extends JpaRepository<Chat, Long> {
     Chat findBysessionId(String seesionId);
+
+    List<Chat> findAllByChallengeId(Long challengeId);
 }
