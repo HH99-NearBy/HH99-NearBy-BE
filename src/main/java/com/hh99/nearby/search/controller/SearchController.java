f98115df15ba42591540f6281da13e4c5af5ff10
nohwooyeol <109066785+nohwooyeol@users.noreply.github.com>
2022-09-27 22:47:35 +0900

    feat(연관검색어): 연관검색어 기능 #139

--- src/main/java/com/hh99/nearby/search/controller/SearchController.java
+++ src/main/java/com/hh99/nearby/search/controller/SearchController.java
@@ -11,9 +11,14 @@ public class SearchController {
 
     private final SearchService searchService;
 
-    @GetMapping("/api/search")
+    @GetMapping("/api/search") //검색기능
     public ResponseEntity<?> search(@RequestParam String keyword,
                                     @RequestParam int pageNum){
         return searchService.search(keyword,pageNum);
+    }
+
+    @GetMapping("/api/relation") //연관검색어
+    public ResponseEntity<?> relationWord(@RequestParam String word){
+        return searchService.relationWord(word);
     }
 }
--- src/main/java/com/hh99/nearby/security/SecurityConfiguration.java
+++ src/main/java/com/hh99/nearby/security/SecurityConfiguration.java
@@ -51,7 +51,8 @@ public class SecurityConfiguration {
             "/api/rank",
             "/api/posts",
             "/",
-            "/api/search"
+            "/api/search",
+            "/api/relation"
     };
     @Bean
     public PasswordEncoder passwordEncoder() {
